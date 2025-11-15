9e597c49dfb1f94d9074af5ac75be67e3f9ab257
BryanAV2008 <bryanandresandrade@gmail.com>
2025-11-15 07:40:01 -0500

    Decimo commit

--- GAMETRAKER-Backend/src/config/db.js
+++ GAMETRAKER-Backend/src/config/db.js
@@ -7,9 +7,7 @@ dotenv.config(); // Carga las variables de entorno desde .env
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      // Opciones de conexión recomendadas (pueden variar ligeramente en versiones futuras de Mongoose)
-      // useNewUrlParser: true, // Ya no es necesario en Mongoose 6+
-      // useUnifiedTopology: true, // Ya no es necesario en Mongoose 6+
+
     });
     console.log(`MongoDB Conectado: ${conn.connection.host}`);
   } catch (error) {
