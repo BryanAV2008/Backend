2364ab0f78a58b307712c685fe69bbcc25461c71
BryanAV2008 <bryanandresandrade@gmail.com>
2025-11-18 18:22:03 -0500

    Decimotercer commit :)

--- GAMETRAKER-Backend/src/server.js
+++ GAMETRAKER-Backend/src/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js';
 
 dotenv.config(); // Carga las variables de entorno
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 5000; 
 
 // Conectar a la base de datos
 connectDB();
